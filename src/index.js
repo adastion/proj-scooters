4c60666163e84fa74755fa5d6edf55f79aee7e63
Andrey Sery <andrey.72006@gmail.com>
2025-02-12 14:55:48 +0300

    started filter for section "cotalog"

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import 'lite-youtube-embed/src/lite-yt-embed.css';
 import 'bootstrap/js/dist/button';
 // import 'bootstrap/js/dist/carousel';
 import 'bootstrap/js/dist/collapse';
-//import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/dropdown';
 import bootstrapModal from 'bootstrap/js/dist/modal';
 // import 'bootstrap/js/dist/offcanvas';
 //import 'bootstrap/js/dist/popover';
