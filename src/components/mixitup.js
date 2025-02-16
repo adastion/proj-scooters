0a05d1ed4cf9655c4aecf95b3c1d7aea0e3b930c
Andrey Sery <andrey.72006@gmail.com>
2025-02-16 12:04:01 +0300

    added mixitup-multifilter

--- src/components/mixitup.js
+++ src/components/mixitup.js
@@ -1,7 +1,14 @@
 import mixitup from 'mixitup';
+import mixitupMultifilter from 'mixitup-multifilter';
+
+mixitup.use(mixitupMultifilter);
 
 const initMixitup = () => {
-        let mixer = mixitup('.filter-block');
+        let mixer = mixitup('.filter-block', {
+                multifilter: {
+                        enable: true, // enable the multifilter extension for the mixer
+                },
+        });
 };
 
 export default initMixitup;
